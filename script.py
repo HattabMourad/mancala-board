80f5a452d32b5fc348b5dc5b8444f08411c5532b
Hattab Omar Mourad <hattabmourad03@gmail.com>
2024-12-08 23:28:16 +0100

    added game over logic and winner evaluation to MancalaGame class

--- script.py
+++ script.py
@@ -44,9 +44,50 @@ class MancalaBoard:
 
             if current_pit in (self.player1_pits if player == 1 else self.player2_pits) and self.board[current_pit] == 1:
                 opposite_pit = self.opponent_pits[current_pit]
-                if self.board[opposite_pit] > 0:
+                if  self.board[opposite_pit] > 0:
                     self.board[player] += self.board[opposite_pit] + 1
                     self.board[opposite_pit] = 0
                     self.board[current_pit] = 0
 
             return current_pit == player 
+    def isGameOver(self):
+        return all(self.board[pit] == 0 for pit in self.player1_pits) or all(self.board[pit] == 0 for pit in self.player2_pits)
+
+    def collectRemainingSeeds(self):
+        for pit in self.player1_pits:
+            self.board[1] += self.board[pit]
+            self.board[pit] = 0
+        for pit in self.player2_pits:
+            self.board[2] += self.board[pit]
+            self.board[pit] = 0
+
+class MancalaGame:
+    def __init__(self, player1_side="human"):
+        self.state = MancalaBoard()
+
+        self.playerSide = {
+            "HUMAN": 1 if player1_side == "human" else 2,
+            "COMPUTER": 1 if player1_side == "computer" else 2
+        }
+
+    def gameOver(self):
+        if self.state.isGameOver():
+            self.state.collectRemainingSeeds()
+            return True
+        return False
+
+    def findWinner(self):
+        human_store = self.state.board[self.playerSide["HUMAN"]]
+        computer_store = self.state.board[self.playerSide["COMPUTER"]]
+
+        if human_store > computer_store:
+            return "HUMAN", human_store
+        elif computer_store > human_store:
+            return "COMPUTER", computer_store
+        else:
+            return "TIE", human_store
+
+    def evaluate(self):
+        computer_store = self.state.board[self.playerSide["COMPUTER"]]
+        human_store = self.state.board[self.playerSide["HUMAN"]]
+        return computer_store - human_store
